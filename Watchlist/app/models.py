207dfd287ed2b00d874e4e0fbda0df8df37ea820
Joy Kirii <joykirii@gmail.com>
2021-04-20 10:46:15 +0300

    restrucuture app

--- Watchlist/app/models.py
+++ Watchlist/app/models.py
@@ -1,5 +1,19 @@
-class Review:
+class Movie:
+  '''
+  Movie class to define Movie Objects
+  '''
 
+  def __init__(self,id,title,overview,poster,vote_average,vote_count):
+    self.id =id
+    self.title = title
+    self.overview = overview
+    self.poster = "https://image.tmdb.org/t/p/w500/" + poster
+    self.vote_average = vote_average
+    self.vote_count = vote_count
+
+
+
+class Review:
   all_reviews = []
 
   def __init__(self,movie_id,title,imageurl,review):
@@ -16,9 +30,10 @@ class Review:
   @classmethod
   def clear_reviews(cls):
     Review.all_reviews.clear()
-  
+
   @classmethod
   def get_reviews(cls,id):
+
     response = []
 
     for review in cls.all_reviews:
